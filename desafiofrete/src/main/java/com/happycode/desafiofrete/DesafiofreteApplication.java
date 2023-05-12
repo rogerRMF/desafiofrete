533aafa76f8976a39c5bf39ab71f62e494904ba3
Rogério Ferreira <rogerioferreira1206@gmail.com>
2023-05-12 17:35:08 -0300

    Project created

---
+++ desafiofrete/src/main/java/com/happycode/desafiofrete/DesafiofreteApplication.java
@@ -0,0 +1,13 @@
+package com.happycode.desafiofrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiofreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiofreteApplication.class, args);
+	}
+
+}
---
+++ desafiofrete/src/test/java/com/happycode/desafiofrete/DesafiofreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happycode.desafiofrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiofreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
